5eaf5531b0c1530f2205d542e636487dc98f543a
alaconte <alaconte@vt.edu>
2022-11-27 14:56:29 -0500

    changed training to use image generators to allow for augmentation of dataset

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.data import AUTOTUNE
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import RMSprop
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import cv2
 import os
 import pandas as pd
@@ -12,34 +12,48 @@ import numpy as np
 from preprocess import resize_width, resize_height
 
 data_dir = './dataset/'
-batch_size = 64
+batch_size = 32
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(resize_height, resize_width),
-    batch_size=batch_size)
+# train_ds = tf.keras.utils.image_dataset_from_directory(
+#     data_dir,
+#     validation_split=0.2,
+#     subset="training",
+#     seed=123,
+#     image_size=(resize_height, resize_width),
+#     batch_size=batch_size)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(resize_height, resize_width),
-  batch_size=batch_size)
+# val_ds = tf.keras.utils.image_dataset_from_directory(
+#   data_dir,
+#   validation_split=0.2,
+#   subset="validation",
+#   seed=123,
+#   image_size=(resize_height, resize_width),
+#   batch_size=batch_size)
 
 AUTOTUNE = tf.data.AUTOTUNE
 
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+datagen = ImageDataGenerator(horizontal_flip=True, zoom_range=(1, 1.5),
+                                    validation_split=0.2)
 
-num_classes = 2
+train_generator = datagen.flow_from_directory(
+        data_dir, 
+        target_size=(resize_width, resize_height), 
+        batch_size=batch_size,
+        subset="training",
+        class_mode='binary')
+
+val_generator = datagen.flow_from_directory(
+        data_dir, 
+        target_size=(resize_width, resize_height), 
+        batch_size=batch_size,
+        subset="validation",
+        class_mode='binary')
+
+# train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Rescaling(1./255),
-    tf.keras.layers.Input(shape=(resize_height, resize_width, 3)),
+    tf.keras.layers.Rescaling(1./255, input_shape=(resize_height, resize_width, 3)),
     tf.keras.layers.Conv2D(16, 3, activation='relu'),
     tf.keras.layers.MaxPooling2D(2),
     tf.keras.layers.Conv2D(32, 3, activation='relu'),
@@ -69,9 +83,13 @@ callbacks = [
     model_checkpoint_callback,
 ]
 
-history = model.fit(train_ds, epochs=600,
-                    validation_data=val_ds,
-                    callbacks=callbacks)
+# history = model.fit(train_ds, epochs=600,
+#                     validation_data=val_ds,
+#                     callbacks=callbacks)
+
+history = model.fit(train_generator, epochs=600,
+                    callbacks=callbacks,
+                    validation_data=val_generator)
 
 model.save("model.h5")
 
